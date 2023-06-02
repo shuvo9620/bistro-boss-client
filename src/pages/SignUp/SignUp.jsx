6ec686dc6e7c48c0e5db95e68aba5a18d727ca67
shuvo9620 <smahfuzur0@gmail.com>
2023-06-03 01:29:13 +0600

    sign up form validation done by react hook form

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -27,13 +27,21 @@ const SignUp = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" {...register("email")} name="email" placeholder="email" className="input input-bordered" />
+                            <input type="email" {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+                            {errors.email && <span className="text-red-600">Email is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" {...register("password")} name="password" placeholder="password" className="input input-bordered" />
+                            <input type="password" {...register("password", {
+                                required: true,
+                                minLength: 6,
+                                maxLength: 10,
+                                pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/
+                            })} name="password" placeholder="password" className="input input-bordered" />
+                            {errors.password && <span className="text-red-600">Password is required</span>}
+                            {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have minimum 6 characters and one number</p>}
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
